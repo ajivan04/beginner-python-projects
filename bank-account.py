d6a716501cfbb68dc72633d0289936852a322444
Abbas Jivan <104444844+ajivan04@users.noreply.github.com>
2022-10-27 10:29:45 -0400

    Update bank-account.py

--- bank-account.py
+++ bank-account.py
@@ -3,23 +3,23 @@ class BankAccount(object):
   def __init__(self, name):
     self.name = name
   def __repr__(self):
-    print "This account belongs to %s and contains $%.2f" % (self.name, self.balance)
+    print("This account belongs to %s and contains $%.2f") % (self.name, self.balance)
   def show_balance(self):
-    print "User Balance: $%.2f" % (self.balance)
+    print("User Balance: $%.2f") % (self.balance)
   def deposit(self, amount):
     if amount <= 0:
-      print "Error, cannot deposit this amount. Please try again."
+      print("Error, cannot deposit this amount. Please try again.")
       return
     else:
-      print "Depositing: $%.2f" % (amount)
+      print("Depositing: $%.2f") % (amount)
       self.balance += amount
       self.show_balance()
   def withdraw(self, amount):
       if amount > self.balance:
-        print "Error, you cannot withdraw more than your current balance."
+        print("Error, you cannot withdraw more than your current balance.")
         return
       else:
-        print "Withdrawing: $%.2f" % (amount)
+        print("Withdrawing: $%.2f") % (amount)
         self.balance -= amount
         self.show_balance()
 
